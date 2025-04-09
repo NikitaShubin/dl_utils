8effaed0d9e2467d2756d19cf0b86b43a9ef2052
Никита Шубин <shubin.kit@ya.ru>
2025-04-09 21:00:30 +0300

    Добавлен pycuda

--- docker/run.sh
+++ docker/run.sh
@@ -79,7 +79,7 @@ RUNPARAMS=(
     # Путь до датасета подменяем локальной его копией:
     # -v "$home/projects/AP2.0/local_files/ds/":"/workspace/data/processed"
     -v "$home/projects/IQF/local_files/ds/":"/workspace/data/processed"
-    -v "/":"/outroot"
+    #-v "/":"/outroot"
 
     # Имя контейнера:
     --name "${DOCKER_NAME}"
